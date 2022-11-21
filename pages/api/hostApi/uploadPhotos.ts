1d7bf94e04c34a951463610888c294829f065368
sangchoonjung <109258408+sangchoonjung@users.noreply.github.com>
2022-11-21 10:21:52 +0900

    22

--- pages/api/hostApi/uploadPhotos.ts
+++ pages/api/hostApi/uploadPhotos.ts
@@ -3,6 +3,7 @@ import { NextApiHandler, NextConfig } from "next";
 import { initializeApp } from "firebase/app";
 import { ref, getStorage, getDownloadURL, uploadBytes } from "firebase/storage";
 import fs from "fs";
+import HostDB from "../../../lib/model/schema/hostSchema";
 
 //multipart.form-data parser는 기본내장이 안되어 있어서 추가 설정해야함
 //firmidable 라는 multipart parsing 라이브러리 활용
@@ -32,58 +33,40 @@ const uploadHandler: NextApiHandler = async (req, res) => {
   // console.log(req.body);
   const form = formidable({ multiples: true });
 
-  const a = [];
-  const result = await new Promise((resolve, rejsct) => {
-    form.parse(req, (err, fields, files) => {
-      resolve({
-        itemId: fields.itemId as string,
-        photos: files.photos as formidable.File[],
+  const result: { itemId: string; photos: formidable.File[] } =
+    await new Promise((resolve, rejsct) => {
+      form.parse(req, (err, fields, files) => {
+        resolve({
+          itemId: fields.itemId as string,
+          photos: files.photos as formidable.File[],
+        });
       });
     });
-  });
   console.log(result);
-  const { itemId, photos } = result;
+
   const storage = getStorage(app);
-  const dirRef = ref(storage, "hosting/" + itemId);
-  const r = photos.map(async (one) => {
+  const dirRef = ref(storage, "hosting/" + result.itemId);
+  const photos: { extraUrl: string }[] = [];
+  for (let one of result.photos) {
     const fileRef = ref(dirRef, one.newFilename);
     const file = fs.readFileSync(one.filepath);
-    //   console.log(file);
-    const result = await uploadBytes(fileRef, file, {
+    await uploadBytes(fileRef, file, {
       //타입지정
       contentType: one.mimetype!,
     });
-    //   console.log(result);
-    return await getDownloadURL(fileRef);
-  });
-  const ww = a.push(r);
-  console.log(ww);
+    photos.push({ extraUrl: await getDownloadURL(fileRef) });
+  }
 
-  //   form.parse(req, async (err, fields, files) => {
-  //     if (err) {
-  //       return console.log("ERROR!!!!!!!", err);
-  //     }
-  //     // console.log(fields);
-  //     // console.log(files);
-  //     const storage = getStorage(app);
-  //     // const dirRef = ref(storage, "hosting/" + fields.itemId);
+  console.log(photos);
 
-  //     // const fileRef = ref(dirRef, one.newFilename);
+  const reaultItem = await HostDB.findByIdAndUpdate(
+    result.itemId,
+    { picture: photos },
+    {
+      returnDocument: "after",
+    }
+  );
 
-  //     for (let one of files.photos as formidable.File[]) {
-  //       //파일 읽기(파일상태로 만듬)
-  //       const file = fs.readFileSync(one.filepath);
-  //       //   console.log(file);
-  //       const result = await uploadBytes(fileRef, file, {
-  //         //타입지정
-  //         contentType: one.mimetype!,
-  //       });
-  //       //   console.log(result);
-  //       const url = await getDownloadURL(fileRef);
-  //       //   console.log(url);
-  //     }
-  //   });
-  //   console.log(PhotoList);
-  return res.status(200).json({});
+  return res.status(200).json(reaultItem);
 };
 export default uploadHandler;
